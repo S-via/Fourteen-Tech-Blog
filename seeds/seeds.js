3213475a4f067abd07f7c81bab4fd6420914e690
S-via <168160925+S-via@users.noreply.github.com>
2024-10-04 15:06:07 -0700

    pseduo code to seeds.js

---
+++ seeds/seeds.js
@@ -0,0 +1,14 @@
+const sequelize = require('../config/connection');
+const {Blog,User} = require('../models');
+
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({force:true});
+    const users = await User.bulkCreate(userData,{
+
+    });
+
+
+}
