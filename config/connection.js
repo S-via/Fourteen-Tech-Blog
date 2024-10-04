f32d51cee23c3344ee27054e8857ace83b85e2d4
S-via <168160925+S-via@users.noreply.github.com>
2024-10-04 15:49:31 -0700

    syntax for line 12 in connection.js

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.DB_URL) {
 } else {
     sequelize = new Sequelize(
         process.env.DB_NAME,
-        prcoess.env.DB_USER,
+        process.env.DB_USER,
         process.env.DB_PASSWORD,
         {
             host: 'localhost',
