8d6034331617050ca917a116e96f1b74ff94d3b9
S-via <168160925+S-via@users.noreply.github.com>
2024-10-05 14:59:11 -0700

    added pathway to sequelize

--- models/index.js
+++ models/index.js
@@ -1,1 +1,17 @@
 // model exports
+const {Model, Dataypes} = require('sequelize');
+const sequelize = require('');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    checkPassword(loginPw){
+        return bcrypt.compareSync(loginPw, this.pasword);
+    }
+}
+
+User.init({
+    user:
+    {
+        type: DataTypes.
+    }
+})
