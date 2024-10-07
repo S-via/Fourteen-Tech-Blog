d3f96555bd2022296951f05cdac9b11440483e29
S-via <168160925+S-via@users.noreply.github.com>
2024-10-07 01:14:40 -0700

    psedu code for routes to be established

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars')
-const routes = require('./controllers');
+const routes = require('./controllers/api');
 
 // not sure if needed
 /* const helpers = require('utils') */
 
-const sequelize = require('.config/connection');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -31,13 +31,13 @@ app.use(session(sess));
 
 // to inform which temples from handlebars to use from express.js
 app.engine('handlebars',hbs.engine);
-app.set('view engine, handlebars');
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,'public')));
 
-app.use(routes);
+app.use(routes); // find right path to routes 
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`App listening ${PORT}`));
