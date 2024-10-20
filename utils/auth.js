740118bab894def35904a7facabf2714191c73e2
S-via <168160925+S-via@users.noreply.github.com>
2024-10-19 18:21:01 -0700

    syntax error in auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,8 @@
 // if user not logged in, redirect them 
 
-// SYNTAX ERROR IN LOOGED_ID >> loggedIn
 const withAuth = (req,res,next) =>{
-    if (!req.session.logged_id){ 
-        res.direct('/login');
+    if (!req.session.logged_in){ 
+        res.redirect('/login');
 
     }else {
         next();
