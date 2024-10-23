6b20bb73e1f273e3caf3be00030c9b57dc8e1c3b
S-via <168160925+S-via@users.noreply.github.com>
2024-10-23 13:21:04 -0700

    added dashboard routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,9 +4,11 @@ const router = require('express').Router();
 const blogRoutes = require('./blogRoutes');
 const userRoutes = require('./userRoutes');
 const commentRoutes = require('./commentRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
 
 router.use('/blogs',blogRoutes);
 router.use('/users',userRoutes);
 router.use('/comments',commentRoutes);
+router.use('/dashboard',dashboardRoutes);
 
 module.exports = router;
