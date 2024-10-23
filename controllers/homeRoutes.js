df2df44bf2873223d1356119748c7784744d81bb
S-via <168160925+S-via@users.noreply.github.com>
2024-10-23 13:40:47 -0700

    create inside homeRoute /dashboardroute

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,42 +4,57 @@ const { User, Blog, Comment } = require('../models/');
 const withAuth = require('../utils/auth');
 
 //// get blogs data ////
-router.get('/',async(req,res)=>{
-        const blogdata = await Blog.findAll({
-            include:[
-                {
-                    model: User,
-                    attributes: ['username'] // Fetch the username
-                }
-            ]}
-        );
+router.get('/', async (req, res) => {
+    const blogdata = await Blog.findAll({
+        include: [
+            {
+                model: User,
+                attributes: ['username'] // Fetch the username
+            }
+        ]
+    }
+    );
 
-        // serilize data and only get the info we need
-        const blogposts = blogdata.map(data => data.get({plain:true}))
-        console.log(blogposts);
+    // serilize data and only get the info we need
+    const blogposts = blogdata.map(data => data.get({ plain: true }))
+    console.log(blogposts);
 
-        // pass blogpost data 
-        res.render('homepage',{
-            blogposts: blogposts,
-            logged_in: req.session.logged_in
-        })
-    });
+    // pass blogpost data to homepage.hdb
+    res.render('homepage', {
+        blogposts: blogposts,
+        logged_in: req.session.logged_in
+    })
+});
 
-    //// get signup route and render to signup page ////
-    router.get('/signup',(req,res)=>{
-        res.render('signup')
-    });
-    
-    //// get login route and render to login page ////
-    router.get('/login',(req,res)=>{
-        if(req.session.logged_in){
-            res.redirect('/dashboard');
-        } else {
+//// get signup route and render to signup page ////
+router.get('/signup', (req, res) => {
+    res.render('signup')
+});
+
+//// get login route and render to login page ////
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+    } else {
         res.render('login');
     }
+});
+
+//// get dashboard route if user is loggedin ////
+router.get('/dashboard', withAuth, async (req, res) => {
+
+    const userblogs = await Blog.findAll({
+        user_id: req.sessio.user_id
     });
 
-    //// get dashboard route if user is loggedin ////
+    const userpost = userblogs.map(data => data.get({ plain: true }));
+    console.log(userpost);
+
+    res.render('dashboard', {
+        userpost: userpost,
+        logged_in: req.session.logged_in
+    });
+});
 
 
 
--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,7 @@
 // if user not logged in, redirect them 
 
 const withAuth = (req,res,next) =>{
-    if (!req.session.loggedIn){ 
+    if (!req.session.logged_in){ 
         res.redirect('/');
 
     }else {
