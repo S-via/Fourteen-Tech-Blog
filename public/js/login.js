7506e8f2f93e440625148c8895ae785e2d2503b5
S-via <168160925+S-via@users.noreply.github.com>
2024-10-27 12:42:53 -0700

    fixed syntax error for login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,32 +1,32 @@
 // event handler to ger login information
 async function loginFormH(event) {
     event.preventDefault();
-}
-
-// to get from login.hdb  passed inside ()
-const username = document.querySelector('#usernameli').value.trim();
-const password = document.querySelector('#passwordli').value.trim();
-
-// when form has values send POST request
-if (username && password) {
-    const response = await fetch('/api/login', {
-        method: 'POST',
-        body: JSON.stringify({
-            username,
-            password
-        }),
-        header: {
-            'Content-type': 'application/json'
+    
+    // to get from login.hdb  passed inside ()
+    const username = document.querySelector('#usernameli').value.trim();
+    const password = document.querySelector('#passwordli').value.trim();
+    
+    // when form has values send POST request
+    if (username && password) {
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            header: {
+                'Content-type': 'application/json'
+            }
+        });
+        if(response.ok){
+            console.log('sucessfull');
+            // redirect to dashboard?? 
+            document.location.replace('/dashboard')
+        } else {
+            // alert response
+            alert(response.status.Text);
         }
-    });
-    if(response.ok){
-        console.log('sucessfull');
-        // redirect to dashboard?? 
-        document.location.replace('/dashboard')
-    } else {
-        // alert response
-        alert(response.status.Text);
     }
 }
-document.querySelector('#loginform')
+    document.querySelector('#loginform')
 .addEventListener('submit',loginFormH);
